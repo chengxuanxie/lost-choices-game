b047fd5ef7c2c3288a839abb5fec1e86c1e64e68
chengxuanxie <504162491@qq.com>
2026-03-10 00:08:34 +0800

    Update registerSW.js

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/lost-choices-game/sw.js', { scope: '/lost-choices-game/' })})}
